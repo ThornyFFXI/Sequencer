5b368fdbec7b7972f3426e7ada6da2d566ba6ec1
ThornyFFXI <10786796+ThornyFFXI@users.noreply.github.com>
2023-11-17 13:34:01 -0500

    Initial commit

---
+++ Sequencer.h
@@ -0,0 +1,54 @@
+#ifndef __ASHITA_Sequencer_H_INCLUDED__
+#define __ASHITA_Sequencer_H_INCLUDED__
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include "C:\Ashita 4\plugins\sdk\Ashita.h"
+
+class Sequencer : IPlugin
+{
+private:
+    IAshitaCore* m_AshitaCore;
+    ILogManager* m_LogManager;
+    IDirect3DDevice8* m_Direct3DDevice;
+    uint32_t m_PluginId;
+    uint32_t mActiveSequence;
+
+public:
+    const char* GetName(void) const override
+    {
+        return "Sequencer";
+    }
+    const char* GetAuthor(void) const override
+    {
+        return "Thorny";
+    }
+    const char* GetDescription(void) const override
+    {
+        return "Modifies packet sequence IDs to prevent actions from Ashitacast/LuAshitacast from double-registering.";
+    }
+    const char* GetLink(void) const override
+    {
+        return "Insert link here.";
+    }
+    double GetVersion(void) const override
+    {
+        return 1.00f;
+    }
+    int32_t GetPriority(void) const override
+    {
+        return 0;
+    }
+    uint32_t GetFlags(void) const override
+    {
+        return (uint32_t)Ashita::PluginFlags::None;
+    }
+	
+    bool Initialize(IAshitaCore* core, ILogManager* logger, const uint32_t id) override;
+    void Release(void) override;
+    void PreProcess(bool outgoing, uint32_t size, uint8_t* data);
+    void PostProcess(bool outgoing, uint32_t size, uint8_t* data);
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,120 @@
+#include "Sequencer.h"
+#pragma comment(lib, "psapi.lib")
+#include <psapi.h>
+#include "thirdparty/detours.h"
+#pragma comment(lib, "thirdparty/detours.lib")
+
+Sequencer* pPlugin = 0;
+
+extern "C"
+{
+    auto Ashita_ParsePackets = static_cast<uint32_t(__thiscall*)(void*, bool, uint32_t, uint8_t*)>(nullptr);
+}
+
+uint32_t __fastcall Mine_ParsePackets(void* pPacketManager, void* pEDX, bool outgoing, uint32_t size, uint8_t* data)
+{
+    pPlugin->PreProcess(outgoing, size, data);
+    size = Ashita_ParsePackets(pPacketManager, outgoing, size, data);
+    pPlugin->PostProcess(outgoing, size, data);
+
+    return size;
+}
+
+__declspec(dllexport) IPlugin* __stdcall expCreatePlugin(const char* args)
+{
+    UNREFERENCED_PARAMETER(args);
+
+    return (IPlugin*)(new Sequencer());
+}
+
+__declspec(dllexport) double __stdcall expGetInterfaceVersion(void)
+{
+    return ASHITA_INTERFACE_VERSION;
+}
+
+bool Sequencer::Initialize(IAshitaCore* core, ILogManager* logger, const uint32_t id)
+{
+    pPlugin      = this;
+    m_AshitaCore = core;
+    m_LogManager = logger;
+    m_PluginId   = id;
+
+    // Get the address to ashita's detoured packet handler..
+    auto ptrEncryptBuffer     = m_AshitaCore->GetPointerManager()->Get("packets.encryptbuffer");
+    DWORD trampolineOffset    = *(int32_t*)((uint8_t*)ptrEncryptBuffer + 1);
+    uintptr_t trampolinedCall = ptrEncryptBuffer + 5 + trampolineOffset;
+
+    // Offset to ashita's packet parsing function..
+    uintptr_t parseCall       = trampolinedCall + 0x20;
+    uintptr_t parseAddress    = parseCall + 5 + *(int32_t*)((uint8_t*)parseCall + 1);
+    Ashita_ParsePackets       = (decltype(Ashita_ParsePackets)(parseAddress));
+
+    // Detour ashita's packet parsing function..
+    ::DetourTransactionBegin();
+    ::DetourUpdateThread(::GetCurrentThread());
+    ::DetourAttach(&(PVOID&)Ashita_ParsePackets, Mine_ParsePackets);
+    ::DetourTransactionCommit();
+
+    return true;
+}
+
+void Sequencer::Release(void)
+{
+    // Restore packet parsing function to normal.
+    ::DetourTransactionBegin();
+    ::DetourUpdateThread(::GetCurrentThread());
+    ::DetourDetach(&(PVOID&)Ashita_ParsePackets, Mine_ParsePackets);
+    ::DetourTransactionCommit();
+}
+
+void Sequencer::PreProcess(bool outgoing, uint32_t size, uint8_t* data)
+{
+    if (!outgoing)
+        return;
+
+    mActiveSequence = 0xFFFFFFFF;
+    auto offset = outgoing ? 0 : 0x1C;
+    while (offset < size)
+    {
+        auto packet = data + offset;
+        auto pid   = (uint16_t)(*(uint16_t*)packet & 0x01FF);
+        auto psize = (uint32_t)(*(uint16_t*)packet >> 0x09) * 0x04;
+
+        // Detect action packets in the initial game-provided chunks.
+        if (pid == 0x1A)
+            mActiveSequence = *((uint16_t*)(packet + 2));
+
+        offset += psize;
+    }
+}
+
+void Sequencer::PostProcess(bool outgoing, uint32_t size, uint8_t* data)
+{
+    if ((mActiveSequence == 0xFFFFFFFF) || (!outgoing))
+        return;
+
+    auto offset = outgoing ? 0 : 0x1C;
+    while (offset < size)
+    {
+        auto packet = data + offset;
+        auto pid   = (uint16_t)(*(uint16_t*)packet & 0x01FF);
+        auto psize = (uint32_t)(*(uint16_t*)packet >> 0x09) * 0x04;
+
+        // Detect action and equip packets in the post-ashita chunks.
+        if ((pid == 0x1A) || (pid == 0x50) || (pid == 0x51))
+        {
+            #ifdef DEBUG_MODE
+            auto sequence = *((uint16_t*)(packet + 2));
+            if (sequence != mActiveSequence)
+            {
+                m_AshitaCore->GetChatManager()->Writef(0, false, "Edited sequence.  %04X -> %04X", sequence, mActiveSequence);
+                *((uint16_t*)(packet + 2)) = mActiveSequence;
+            }
+            #else
+            *((uint16_t*)(packet + 2)) = mActiveSequence;
+            #endif
+        }
+
+        offset += psize;
+    }
+}
